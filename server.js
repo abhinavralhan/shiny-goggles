e82fddaec4698de7161c2402026369dbc4d0dbe0
abhinav ralhan <abhinav.ralhan1@gmail.com>
2017-07-12 15:50:55 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+
+// Get dependencies
+const express = require('express')
+const path = require('path')
+const http = require('http')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const passport = require('passport')
+const cookieParser = require('cookie-parser')
+//const [a,b] = require('./server/models/schema');
+
+
+//console.log(' secret here' + b)
+// Get our API routes
+const app = express()
+//const api = require('./server/routes/api')
+
+/*app.use(passport.initialize())
+app.use(passport.session())
+
+require('./server/passport')(passport)
+*/
+// Parsers for POST data
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(cookieParser(config.cookieSecret)) //cookies of the current Session
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')))
+
+// Set our api routes
+//app.use('/',api)
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './my-app/dist/index.html'))
+});
+
+
+//Get port from environment and store in Express
+const port = process.env.PORT || '8080'
+app.set('port', port)
+
+
+//Create HTTP server
+const server = http.createServer(app)
+
+//Listen on provided port, on all network interfaces.
+server.listen(port, () => console.log(`API running on localhost:${port}`))
