08d9794540b408d4fc0de07564576ca0077aef0f
abhinav ralhan <abhinav.ralhan1@gmail.com>
2017-07-13 16:12:39 +0530

    server #4 schema and package fixes

--- server/models/schema.js
+++ server/models/schema.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose')
 const assert = require('assert')
-const validate = require('mongoose-validator').validate;
+const validate = require('mongoose-validator');
 
 let dbConnection = mongoose.createConnection('mongodb://localhost:27017/test')
 
@@ -11,6 +11,21 @@ let schema = mongoose.Schema
 let ObjectId = schema.ObjectId;
 //let secret = 'abcd'
 
+let validator = [
+	validate({
+	    validator: 'isLength',
+	    arguments: [1, 50],
+	    message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters'
+	  })
+	]
+let longValidator = [
+	validate({
+	    validator: 'isLength',
+	    arguments: [1, 250],
+	    message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters'
+	  })
+	]
+
 let patientSchema = new schema({
 
 	id: {
@@ -18,26 +33,26 @@ let patientSchema = new schema({
 	},
 	patient_id: {
 		type: String,
-		required: [true, 'Name required']
+		required: [true, 'Name required'],
 		unique: true
 	},
 	name : {
 		type: String,
 		required: [true, 'Name required'],
-		validate: validate('len', 1, 50)
+		validate: validator
 	},
 	mName : {
 		type: String,
-		validate: validate('len', 1, 15)
-	}
+		validate: validator
+	},
 	lName : {
 		type: String,
 		required: [true, 'Last name required'],
-		validate: validate('len', 1, 50)
-	}
+		validate: validator
+	},
 	ssn : {
 		type: String,
-	    validate: validate('len', 1, 20)	
+	    validate: validator
 	},
 	dob : {
 		type: Date,
@@ -46,32 +61,32 @@ let patientSchema = new schema({
 	gender : {
 		type: String,
 		required : [true, 'Gender required'],
-	    validate: validate('len', 1, 25)	
-	}
+	    validate: validator
+	},
 	maritalStatus : {
 		type : String,
 		required : [true, 'Marital status required'],
-	    validate: validate('len', 1, 15)
+	    validate: validator
 	},
 	address1 : {
 		type: String,
-	    validate: validate('len', 1, 250)
+	    validate: longValidator
 	},
 	address2 : {
 		type: String,
-	    validate: validate('len', 1, 50)
+	    validate: validator
 	},
 	city : {
 		type: String,
-	    validate: validate('len', 1, 50)
+	    validate: validator
 	},
 	state : {
 		type: String,
-	    validate: validate('len', 1, 50)
+	    validate: validator
 	},
 	country : {
 		type: String,
-	    validate: validate('len', 1, 50)
+	    validate: validator
 	},
 	zip_code : {
 		type: String,
@@ -155,9 +170,9 @@ let patientSchema = new schema({
 
 });
 
-patientSchema.virtual('patient_id').get(function() {
+/*patientSchema.virtual('patient_id').get(function() {
     return this._id;
-});
+});*/
 
 /*
 var User = mongoose.model('User', userSchema);
@@ -169,7 +184,7 @@ var json = doc.toJSON({ virtuals: true });
 */
 
 
-let discreteAttributeSchema = new Schema({
+let discreteAttributeSchema = new schema({
 	discrete_attribute_id: {
 		type: ObjectId,
 		unique: true
@@ -179,7 +194,7 @@ let discreteAttributeSchema = new Schema({
 		minLength: 1,
 		maxLength: 30,
 		required: [ true, 'Attribute name required' ]
-	}
+	},
 	attribute_value : {
 		type: String,
 		minLength: 1,
@@ -189,7 +204,7 @@ let discreteAttributeSchema = new Schema({
 	date_created : {
 		type: Date,
 		required: [ true, 'Date required']
-	}
+	},
 	created_by : {
 		type: String,
 		minLength: 1,
@@ -198,7 +213,7 @@ let discreteAttributeSchema = new Schema({
 	},
 	date_modified : {
 		type: Date
-	}
+	},
 	modified_by :{
 		type: String,
 		minLength: 1,
@@ -206,10 +221,10 @@ let discreteAttributeSchema = new Schema({
 	}
 })
 
-atientSchema.virtual('discrete_attribute_id').get(function() {
+/*atientSchema.virtual('discrete_attribute_id').get(function() {
     return this._id;
 });
-
+*/
 /*
 var User = mongoose.model('User', userSchema);
 User.findOne(function(err, doc) {
