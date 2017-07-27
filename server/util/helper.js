3dcd771eef9b7d3102880eb0f1c243414678ff5b
abhinav ralhan <abhinav.ralhan1@gmail.com>
2017-07-27 17:10:29 +0530

    additions made

--- server/util/helper.js
+++ server/util/helper.js
@@ -27,9 +27,13 @@ module.exports.createUsername = function createUsername(data, callback) {
 	//console.log('\n\nuserCollection.patient = ' + userCollection)
 
 	console.log('\ndata ' + data)
-	var query = {patient_id : {$gte: 21, $lte: 25} };
+	var query = { patient_id : '25' };
+	var fullname = "sachin Tendul"
 
-	userCollection.find(query,  (err, result) => {
+    userCollection.find({ query });
+
+
+	userCollection.findOneandUpdate(query, {$set: { username = }}, (err, result) => {
 		if(err){
 			return callback(err,null)
 		}
